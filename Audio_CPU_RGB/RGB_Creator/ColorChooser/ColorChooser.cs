338359f65ca1d31a8659746c0259f348e0063426
Arne <arne-@hotmail.de>
2019-10-29 08:11:10 +0100

    Now fading in ColorChoser. Implementation of producer/consumer

--- Audio_CPU_RGB/RGB_Creator/ColorChooser/ColorChooser.cs
+++ Audio_CPU_RGB/RGB_Creator/ColorChooser/ColorChooser.cs
@@ -1,33 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using AudioCPURGB.RGB_Output;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Windows.Forms;
-using System.Drawing.Imaging;
+﻿using System.Threading;
 using AudioCPURGB.RGB_Creator;
 
 namespace AudioCPURGB
 {
     class ColorChooser : RGB_Creator_Interface
-    {        
+    {
+        RGB_Value old_rgb = new RGB_Value();
+        RGB_Value new_rgb = new RGB_Value();
+        Mutex new_rgb_mutex = new Mutex();
 
         public void rgbChanged(int r, int g, int b)
         {
-            RGB_Value rgb = new RGB_Value((byte)r, (byte)g, (byte)b);
-            //String write =
-            System.Diagnostics.Debug.Print("New RGB: " + rgb.r + ", " + rgb.g + ", " + rgb.b);
-            _rgbOutput.showRGB(rgb);
+            new_rgb_mutex.WaitOne();
+            new_rgb = new RGB_Value((byte)r, (byte)g, (byte)b);
+            new_rgb_mutex.ReleaseMutex();
         }
 
         protected override void callback()
         {
+            RGB_Value copy_val = new RGB_Value();
+            new_rgb_mutex.WaitOne();
+            copy_val.copy_values(new_rgb);
+            new_rgb_mutex.ReleaseMutex();
 
+            _rgbOutput.fade(old_rgb, copy_val, 10);
+            old_rgb = copy_val;
+            Thread.Sleep(100);
         }
     }
 }
