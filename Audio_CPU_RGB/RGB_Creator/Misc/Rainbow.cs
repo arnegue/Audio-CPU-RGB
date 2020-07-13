516a47bea48edb49e7f79efdf19bb5372b4e7f23
Arne <arne-@hotmail.de>
2020-07-13 12:33:52 +0200

    Better Rainbow algorithm

--- Audio_CPU_RGB/RGB_Creator/Misc/Rainbow.cs
+++ Audio_CPU_RGB/RGB_Creator/Misc/Rainbow.cs
@@ -9,16 +9,83 @@ namespace AudioCPURGB
     /// </summary>
     class Rainbow : IndividualRGBOutput
     {
-        Random random = new Random();
+        RGB_Value[] _rainbow_rgbs;
+        int _phase_length;
+        int last_index = 0;
+ 
+        private int[] get_rainbow_values()
+        {
+            int[] rgb_values = new int[amount_rgbs];
+            double slope = 255.0 / ((float)amount_rgbs / 6.0);
+            int idx = 0;
+            int p;
+            for (int i = 0; i < amount_rgbs; i++)
+            {
+                // Phase 1 getting up
+                for (p = 0; p < _phase_length; p++, idx++)
+                {
+                    rgb_values[idx % amount_rgbs] = (int)(slope * p);
+                }
+                // Phase 2 stay up
+                for (p = 0; p < _phase_length * 2; p++, idx++)
+                {
+                    rgb_values[idx % amount_rgbs] = 255;
+                }
+                // Phase 3 getting down
+                for (p = 0; p < _phase_length; p++, idx++)
+                {
+                    rgb_values[idx % amount_rgbs] = rgb_values[_phase_length - p]; // TODO modulo?
+                }
+                // Phase 4 stay down
+                for (p = 0; p < _phase_length * 2; p++, idx++)
+                {
+                    rgb_values[idx % amount_rgbs] = 0;
+                }
+            }
+            return rgb_values;
+        }
+        private void get_rainbow_rgbs()
+        {
+            //int left_rgbs = amount_rgbs % 6;  // consider uneven amount of LEDs
+            _phase_length = amount_rgbs / 6;
+            if (_phase_length < 1)
+            {
+                _phase_length = 1;
+            }
+
+            int[] rainbow_values = get_rainbow_values();
+
+            _rainbow_rgbs = new RGB_Value[amount_rgbs];
+            for (int i = 0; i < amount_rgbs; i++)
+            {
+                byte r, g, b;
+                r = (byte)rainbow_values[(i + _phase_length * 2) % amount_rgbs];
+                g = (byte)rainbow_values[i];
+                b = (byte)rainbow_values[(i + _phase_length * 4) % amount_rgbs];
+
+                _rainbow_rgbs[i] = new RGB_Value(r, g, b);
+            }
+        }
 
         protected override RGB_Value[] callback(RGB_Value[] new_rgbs)
-        {           
-            for (int i = 0; i < old_rgbs.Length; i++)
+        {
+            if (_rainbow_rgbs == null)
             {
-                new_rgbs[i].copy_values(new RGB_Value((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+                get_rainbow_rgbs();
             }
-            _rgbOutput.fade(old_rgbs, new_rgbs, 5);
-            Thread.Sleep(250);
+
+            for (int i = 0; i < amount_rgbs; i++)
+            {
+                new_rgbs[i] = _rainbow_rgbs[(i + last_index) % amount_rgbs];   
+                
+            }
+            last_index++;
+
+            //_rgbOutput.fade(old_rgbs, new_rgbs, 20);
+            //_rgbOutput.showRGBs(new_rgbs);
+            _rgbOutput.fade(old_rgbs, new_rgbs, 20);
+            old_rgbs = new_rgbs;
+            //Thread.Sleep(250);
 
             return new_rgbs;
         }        
